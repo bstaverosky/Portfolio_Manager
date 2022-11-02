03cd2d7525a2e0dfcfeaecf6e4db9600a58ec3a6
bstaverosky <bstaverosky@gmail.com>
2022-11-01 20:19:33 -0400

    11/1/2022 upload

--- portfolio_manager_beta.R
+++ portfolio_manager_beta.R
@@ -14,6 +14,8 @@ library(rvest)
 library(magrittr)
 library(tibble)
 library(quantmod)
+library(plyr)
+
 get_returns <- function(symbols){
   lapply(symbols, FUN = function(x){
     print(x)
@@ -49,8 +51,8 @@ symbols <- gsub("\\.","-", symbols)
 
 #save(prices, file = "/home/brian/Documents/projects/portfolio_manager/prices.RData")
 
-load("/home/brian/Documents/projects/portfolio_manager/prices.RData")
-write.csv(prices, file="/var/lib/mysql-files/prices.csv")
+load("/home/brian/Documents/projects/portfolio_manager_data/prices.RData")
+#write.csv(prices, file="/var/lib/mysql-files/prices.csv")
 
 ### CALCULATE DAILY RETURNS ###
 returns <- lapply(prices, FUN = function(x){
@@ -63,8 +65,7 @@ returns <- lapply(prices, FUN = function(x){
 retdf <- do.call("cbind", returns)
 
 ### SAVE TO MYSQL SERVER ###
-
-write.csv(retdf, file="/home/brian/Documents/projects/portfolio_manager/returns.csv")
+#write.csv(retdf, file="/home/brian/Documents/projects/portfolio_manager_data/returns.csv")
 
 
 ##### GET FUNDAMENTAL INDEX RETURNS #####
@@ -87,7 +88,7 @@ actret <- lapply(returns, FUN = function(x){
 
 # get active factor returns
 
-actret <- lapply(, FUN = function(x){
+fret <- lapply(fbase, FUN = function(x){
   df <- merge(x, bmk)
   df <- df[complete.cases(df),]
   df[,1] <- df[,1]-df[,2]
@@ -95,6 +96,69 @@ actret <- lapply(, FUN = function(x){
 })
 
 # get correlations
+
+# Make function that takes xts of factor return and xts of stock return
+# and returns a data frame of rolling correlations
+
+x <- fret[[1]]
+y <- actret[[1]]
+
+
+
+
+roll_cor <- function(x, y){
+  df <- merge(x, y)
+  df <- df[complete.cases(df),]
+  olst <- list()
+  if(nrow(df)>253){
+    for (j in 253:nrow(df)){
+      #print(j)
+      jdate <- index(df)[[j]]
+      subf <- df[((j-252):j),]
+      cr   <- cor(subf[,1],subf[,2])
+      
+      outdf <- data.frame(correlation = cr)
+      names(outdf) <- names(df)[[2]]
+      row.names(outdf) <- jdate
+      olst[[jdate]] <- outdf
+    }
+  } else {
+    jdate <- tail(index(df),1)
+    outdf <- data.frame(correlation = NA)
+    row.names(outdf) <- jdate
+    olst[[jdate]] <- outdf
+  }
+  outdf <- do.call("rbind", olst)
+  outdf
+}
+
+
+# Quality Scores
+olist <- list()
+x <- fret[[1]]
+  for(i in actret){
+    print(names(i))
+    outp  <- roll_cor(x, i)
+    olist[[names(i)]] <- outp
+  }
+  
+save(olist, file = "/home/brian/Documents/projects/portfolio_manager_data/qualcor.RData")
+
+
+maxrow <- lapply(olist, )
+
+qual <- plyr::ldply(olist, rbind)
+
+qual <- do.call("cbind", olist)
+  
+  
+ 
+
+
+
+
+
+
 
 lapply(actret, FUN = function(x){
   
