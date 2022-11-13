1ca1480601a2a7e990428dd85bbe684773e73214
bstaverosky <bstaverosky@gmail.com>
2022-11-12 20:02:01 -0500

    11/12/2022 commit

--- build_panel_file.R
+++ build_panel_file.R
@@ -21,23 +21,64 @@ load("/home/brian/Documents/projects/portfolio_manager_data/growthdf.RData")
 # Value Scores
 load("/home/brian/Documents/projects/portfolio_manager_data/valuedf.RData")
 
+
+mobs <- min(sapply(list(growth,qual,value), FUN = function(x){nrow(x)}))
+
+
 ### Load from mysql ###
 
-### Split returns by date
-rbyd <- split(tframe, tframe$date)
-rtest <- rbyd[[10000]]
+### CONVERT TO PANELDAT ###
 
-### Convert Factor frame 
-test <- split(qual, row.names(qual))
+retpanel  <- split(tframe, tframe$date)
+retpanel  <- retpanel[(length(retpanel)-(mobs-1)):length(retpanel)]
+qualpanel <- convert_to_panel(qual, "quality")
+qualpanel  <- qualpanel[(length(qualpanel)-(mobs-1)):length(qualpanel)]
+growpanel <- convert_to_panel(growth, "growth")
+valpanel  <- convert_to_panel(value, "value")
 
-tst <- test[[1]]
-qdate <- row.names(tst)[[1]]
-tst <- data.frame(t(tst))
-names(tst) <- "quality"
-tst$ticker <- row.names(tst)
-tst$date <- qdate
-tst <- tst[,c("date", "ticker", "quality")]
 
+### MERGE ALL PANELS ###
+
+panel_list <- list()
+for (i in 1:mobs){
+  print(i)
+  q <- qualpanel[[i]]
+  g <- growpanel[[i]]
+  v <- valpanel[[i]]
+  r <- retpanel[[i]]
+  dflist <- list(q,g,v,r)
+  out <- Reduce(function(x, y) merge(x, y, by = intersect(names(x),names(y))), dflist)
+  panel_list[[i]] <- out
+}
+
+
+
+
+
+
+
+
+
+### CONVERT RAW TO PANEL FORM ###
+convert_to_panel <- function(fdata, fname){
+  if(fname=="dreturn"){
+    y <- split(fdata, fdata$date)
+  } else {
+    y <- split(fdata, row.names(fdata))
+  }
+  plist <- lapply(y, FUN = function(x){
+    #x <- y[[1]]
+    qdate <- row.names(x)[[1]]
+    x <- data.frame(t(x))
+    names(x) <- fname
+    x$ticker <- row.names(x)
+    x$date <- qdate
+    x <- x[,c("date", "ticker", fname)]
+    x <- x[!x$ticker=="correlation",]
+    x
+  })
+  plist
+}
 
 
 
