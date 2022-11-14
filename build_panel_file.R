ae0bbf06203d23633219777b8bb72033a0468b98
bstaverosky <bstaverosky@gmail.com>
2022-11-13 20:20:59 -0500

    11/13/2022 commit

--- build_panel_file.R
+++ build_panel_file.R
@@ -10,6 +10,27 @@ library(magrittr)
 library(tibble)
 library(quantmod)
 library(plyr)
+library(RMySQL)
+library(DBI)
+
+### CONNECT TO MYSQL DATABASE ###
+
+mysqlconnection = dbConnect(RMySQL::MySQL(),
+                            dbname='returns',
+                            host='localhost',
+                            port=3306,
+                            user=rstudioapi::askForPassword("Database user"),
+                            password=rstudioapi::askForPassword("Database password")
+)
+
+
+mysqlconnection = dbConnect(RMySQL::MySQL(),
+                            dbname='returns',
+                            host='localhost',
+                            port=3306,
+                            user='root',
+                            password='M@uricio24'
+                            )
 
 ### Load from RData ###
 #Returns
@@ -21,24 +42,18 @@ load("/home/brian/Documents/projects/portfolio_manager_data/growthdf.RData")
 # Value Scores
 load("/home/brian/Documents/projects/portfolio_manager_data/valuedf.RData")
 
-
+# Maximum number of observations
 mobs <- min(sapply(list(growth,qual,value), FUN = function(x){nrow(x)}))
-
 
 ### Load from mysql ###
 
-### CONVERT TO PANELDAT ###
-
-retpanel  <- split(tframe, tframe$date)
-retpanel  <- retpanel[(length(retpanel)-(mobs-1)):length(retpanel)]
-qualpanel <- convert_to_panel(qual, "quality")
-qualpanel  <- qualpanel[(length(qualpanel)-(mobs-1)):length(qualpanel)]
-growpanel <- convert_to_panel(growth, "growth")
-valpanel  <- convert_to_panel(value, "value")
-
+### CONVERT TO PANELDATA ###
+retpanel  <- convert_to_panel(tframe, "dreturn", mobs)
+qualpanel <- convert_to_panel(qual, "quality", mobs)
+growpanel <- convert_to_panel(growth, "growth", mobs)
+valpanel  <- convert_to_panel(value, "value", mobs)
 
 ### MERGE ALL PANELS ###
-
 panel_list <- list()
 for (i in 1:mobs){
   print(i)
@@ -51,6 +66,10 @@ for (i in 1:mobs){
   panel_list[[i]] <- out
 }
 
+save(panel_list, file = "/home/brian/Documents/projects/portfolio_manager_data/panel_list.RData")
+### CONVERT THE ABOVE INTO A FUNCTION ###
+#merge_panels <- function(plist){
+#}
 
 
 
@@ -58,27 +77,6 @@ for (i in 1:mobs){
 
 
 
-
-### CONVERT RAW TO PANEL FORM ###
-convert_to_panel <- function(fdata, fname){
-  if(fname=="dreturn"){
-    y <- split(fdata, fdata$date)
-  } else {
-    y <- split(fdata, row.names(fdata))
-  }
-  plist <- lapply(y, FUN = function(x){
-    #x <- y[[1]]
-    qdate <- row.names(x)[[1]]
-    x <- data.frame(t(x))
-    names(x) <- fname
-    x$ticker <- row.names(x)
-    x$date <- qdate
-    x <- x[,c("date", "ticker", fname)]
-    x <- x[!x$ticker=="correlation",]
-    x
-  })
-  plist
-}
 
 
 
